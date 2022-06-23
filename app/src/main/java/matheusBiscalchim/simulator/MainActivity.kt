3a38ebe18e763a04ed2145552f4e6be309d66e56
MatheusBiscalchim <matheusbiscalchimmm@gmail.com>
2022-06-23 12:02:32 -0300

    first commit

---
+++ app/src/main/java/matheusBiscalchim/simulator/MainActivity.kt
@@ -0,0 +1,11 @@
+package matheusBiscalchim.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
